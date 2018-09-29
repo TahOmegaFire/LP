587b8768f5aa8cec5da24934f8dbd07c96cf2146
Daniel Bornscheuer <d.bornscheuer69@gmail.com>
2018-09-28 23:04:04 -0300

    Implementado loop, malo pero funca creo

--- lolcode.py
+++ lolcode.py
@@ -2,13 +2,22 @@ import re
 
 #TODO: Declared variable checking, variable assignation, conditionals, loops
 
-vOCRegex = r'([A-Z|a-z]?\w*\_*\w*|\d+)' #Regex for detecting either variables or numeric constants
+vOCRegex = r'([A-Z|a-z](?:\w|\_)*|\d+)' #Regex for detecting either variables or numeric constants
 oVOCRegex = re.compile(vOCRegex)
 #exprRegex = re.compile(r'(((SUM|DIFF|PRODUKT|QUOSHUNT|MOD|BIGGR|SMALLR|BOTH|EITHER) OF )(([A-Z]+ OF .+ AN .+)|'+vOCRegex+r')+ AN (([A-Z]+ OF .+ AN .+)|'+vOCRegex+')+)') #TODO: Rest of operators holy shit that was long
 exprRegex = re.compile(r'(SUM|DIFF|PRODUKT|QUOSHUNT|MOD)? OF (.+) AN (.+)')
 varDeclRegex = re.compile(r'(I HAS A [A-Z|a-z]\w*)\s*(?:ITZ (.+))?')
 expVISRegex=re.compile(r'((VISIBLE|GIMMEH) (.+))') #GIMMEH NO ES LO MISMO QUE EL VISIBLE CAMBIAR
+loopRegex=re.compile(r'IM IN YR ([A-Z|a-z](?:\w|\_)*) (?:UPPIN|NERFIN) YR ([A-Z|a-z](?:\w|\_)*) (?:TIL|WHILE) (.+)')
+loopERegex = re.compile(r'IM OUTTA YR (.+)')
 expRegular=[exprRegex,varDeclRegex,expVISRegex]
+loopLines = list()
+loopNames = list()
+
+nLoop = 0
+pBeg = False
+pEnd = False
+
 def CheckExpr(sLine): #Group 2 is first operand, group 3 is the rest
 	eMatch=None
 	if re.match(expRegular[0],sLine):
@@ -33,38 +42,100 @@ def CheckExpr(sLine): #Group 2 is first operand, group 3 is the rest
 		if re.match(oVOCRegex,sLine).group(1)!=sLine:
 			return False
 	return True
-pBeg = False
-pEnd = False
-eMatch = re.match(varDeclRegex,"I HAS A n")
-if eMatch is not None:
-	print (eMatch.groups())
-else:
-	print("lala")
+
+def Iter(line):
+	global pBeg
+	global pEnd
+	global nLoop
+	global loopNames
+	global loopLines
+	if not pBeg and line != 'HAI':
+		print(line + ' | Code has not correctly begun\n')
+		#continue
+		return
+
+	elif line == 'HAI':
+		print(line + ' | HAJIMERUZO\n')
+		pBeg = True
+		pEnd = False
+		#continue
+		return
+
+	if pEnd == True and line != '':
+		print(line + ' | ERROR: Code post script end')
+		#continue
+		return
+
+	elif line == 'KTHXBYE':
+		print(line + ' | BAI\n')
+		pEnd = True
+		pBeg = False
+		#continue
+		return
+#if re.match(expRegular[0],line) is not None:
+#print(re.match(expRegular[0],line).groups())
+
+	m = re.match(loopRegex, line)
+	if m is not None:
+		loopNames.append(m.group(1))
+		nList = list()
+		loopLines.append(nList)
+		nLoop += 1
+		loopLines[nLoop - 1].append(line)
+		return
+
+	if CheckExpr(line):
+		print(line + ' | Expr\n')
+		return
+	else:
+		print(line + ' | Error\n')
+
+def LoopHelper(loopLines, depth):
+	print(loopLines[depth][0] + ' | Loop begin')
+	
+	length = len(loopLines[depth]) - 1
+	for line in loopLines[depth][1:length]:
+		l = re.match(loopRegex, line)
+		if l is not None:
+			print(l.groups())
+			LoopHelper(loopLines, depth + 1)
+			continue
+		Iter(line)
+	print(loopLines[depth][length] + ' | Loop end')
+
+
 with open('code.lc') as fIn:
-	for line in fIn:
-		line = line.strip()
-		if not pBeg and line != 'HAI':
-			print(line + ' | Code has not correctly begun\n')
-			continue
+	for iLine in fIn:
+		iLine = iLine.strip()
+		if nLoop == 0:
+			if len(loopLines) != 0:
+				LoopHelper(loopLines, 0)
+			Iter(iLine)
 
-		elif line == 'HAI':
-			print(line + ' | HAJIMERUZO\n')
-			pBeg = True
-			continue
-
-		if pEnd == True and line != '':
-			print(line + ' | ERROR: Code post script end')
-			continue
-
-		elif line == 'KTHXBYE':
-			print(line + ' | BAI\n')
-			pEnd = True
-			continue
-		#if re.match(expRegular[0],line) is not None:
-			#print(re.match(expRegular[0],line).groups())
-
-		if CheckExpr(line):
-			print(line + ' | Expr\n')
-			continue
 		else:
-			print(line + ' | Error\n')
+			loopLines[nLoop - 1].append(iLine)
+			e = re.match(loopERegex, iLine)
+			l = re.match(loopRegex, iLine)	
+			if e is not None and loopNames[nLoop - 1] == e.group(1):
+				loopNames.pop()
+				#loopLines[nLoop - 1].pop()
+				nLoop -= 1
+				#print(loopLines[nLoop][0] + ' | Loop begin')
+				#for lLine in loopLines[nLoop][1:]:
+				#	Iter(lLine)
+				#print(iLine + ' | Loop end')
+				#del loopLines[nLoop]
+
+			else:
+				for name in loopNames:
+					if e is not None and name == e.group(1):
+						nLoop -= 1
+						for lLine in loopLines[nLoop]:
+							print(lLine + ' | Error')
+			if l is not None:
+				loopNames.append(l.group(1))
+				nList = list()
+				loopLines.append(nList)
+				nLoop += 1
+				loopLines[nLoop - 1].append(iLine)
+				continue
