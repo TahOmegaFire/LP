902a28ca84dbbe0e6c415e2fa648ee8a6c2e206a
TahOmegaFire <d.bornscheuer69@gmail.com>
2018-10-19 16:49:55 -0300

    Functions 1-5 ready
    Yeeee

--- C/genetico.c
+++ C/genetico.c
@@ -16,40 +16,62 @@ typedef struct listaE
 	struct listaE* next;
 } listaE;
 
+void copyNodo(listaE* nNodo, listaE* cNodo)
+{
+	nNodo->m_Nodo.tipo = cNodo->m_Nodo.tipo;
+	if(nNodo->m_Nodo.tipo == 'i')
+	{
+		nNodo->m_Nodo.dato = malloc(sizeof(int*));
+		*((int *)nNodo->m_Nodo.dato) = *((int *)cNodo->m_Nodo.dato);
+	}
+
+	else if(nNodo->m_Nodo.tipo == 'c')
+	{
+		nNodo->m_Nodo.dato = malloc(sizeof(char*));
+		*((char *)nNodo->m_Nodo.dato) = *((char *)cNodo->m_Nodo.dato);
+	}
+
+	else if(nNodo->m_Nodo.tipo == 'b')
+	{
+		nNodo->m_Nodo.dato = malloc(sizeof(bool*));
+		*((bool *)nNodo->m_Nodo.dato) = *((bool *)cNodo->m_Nodo.dato);
+	}
+}
+
 void setNodo(listaE* h)
 {
+	h->m_Nodo.dato = NULL;
 	unsigned char tipo = rand() % 3;
 	if(tipo == 0)
 	{
 		h->m_Nodo.tipo = 'i';
-		h->m_Nodo.dato = malloc(sizeof(int));
+		h->m_Nodo.dato = malloc(sizeof(int*));
 		(*(int *)h->m_Nodo.dato) = rand() % 10;
 	}
 
 	else if(tipo == 1)
 	{
 		h->m_Nodo.tipo = 'c';
-		h->m_Nodo.dato = malloc(sizeof(char));
+		h->m_Nodo.dato = malloc(sizeof(char*));
 		(*(int *)h->m_Nodo.dato) = 65 + rand() % 6;
 	}
 
 	else if(tipo == 2)
 	{
 		h->m_Nodo.tipo = 'b';
-		h->m_Nodo.dato = malloc(sizeof(bool));
+		h->m_Nodo.dato = malloc(sizeof(bool*));
 		(*(int *)h->m_Nodo.dato) = rand() % 2;
 	}
 }
 
 void* generarSolucion(int n)
 {
-	srand(time(NULL));
 	listaE* head = (listaE*)malloc(sizeof(listaE));
 	head->next = NULL;
 	setNodo(head);
 	listaE* cur = head;
 	
-	for(int i = 0; i < n; ++i)
+	for(int i = 1; i < n; ++i)
 	{
 		listaE* newEn = (listaE*)malloc(sizeof(listaE));
 		newEn->next = NULL;
@@ -57,9 +79,90 @@ void* generarSolucion(int n)
 		cur->next = newEn;
 		cur = newEn;
 	}
+
+	return head;
 }
 
 void* copiar(void* Lista)
 {
+	listaE* nHead = (listaE*)malloc(sizeof(listaE));
+	nHead->next = NULL;
+	nHead->m_Nodo.tipo = ' ';
+	nHead->m_Nodo.dato = NULL;
+	copyNodo(nHead, Lista);
+	listaE* cur = nHead;
+	listaE* curPtr = Lista;
+	while(curPtr->next != NULL)
+	{
+		curPtr = curPtr->next;
+		listaE* newEn = (listaE*)malloc(sizeof(listaE));
+		newEn->next = NULL;
+		newEn->m_Nodo.tipo = ' ';
+		newEn->m_Nodo.dato = NULL;
+		copyNodo(newEn, curPtr);
+		cur->next = newEn;
+		cur = newEn;
+	}
 
+	return nHead;
+}
+
+void borrar(void* Lista)
+{
+	free(((listaE*)Lista)->m_Nodo.dato);
+	if(((listaE*)Lista)->next != NULL)
+	{
+		borrar(((listaE*)Lista)->next);
+		((listaE*)Lista)->next = NULL;
+	}
+
+	free(Lista);
+}
+
+void imprimirSolucion(void* Lista)
+{
+	listaE* cur = (listaE*)Lista;
+	while(cur->next != NULL)
+	{
+		if(cur->m_Nodo.tipo == 'i')
+			printf("(%i, i)\n", *(int*)cur->m_Nodo.dato);
+		else if(cur->m_Nodo.tipo == 'c')
+			printf("(%c, c)\n", *(char*)cur->m_Nodo.dato);
+		else if(cur->m_Nodo.tipo == 'b')
+			printf("(%d, b)\n", *(bool*)cur->m_Nodo.dato);
+
+		cur = cur->next;
+	}
+
+	if(cur->m_Nodo.tipo == 'i')
+		printf("(%i, i)\n", *(int*)cur->m_Nodo.dato);
+	else if(cur->m_Nodo.tipo == 'c')
+		printf("(%c, c)\n", *(char*)cur->m_Nodo.dato);
+	else if(cur->m_Nodo.tipo == 'b')
+		printf("(%d, b)\n", *(bool*)cur->m_Nodo.dato);
+}
+
+void cruceMedio(void* Lista1, void* Lista2)
+{
+	int c = 1;
+	listaE* ptr = (listaE*)Lista1;
+	while(ptr->next != NULL)
+	{
+		++c;
+		ptr = ptr->next;
+	}
+	
+	c /= 2;
+
+	ptr = (listaE*)Lista1;
+	listaE* ptr2 = (listaE*)Lista2; //I dunno how else to do this lmao
+
+	for(int i = 0; i < c; ++i)
+	{
+		tNodo temp = ptr->m_Nodo;
+		ptr->m_Nodo = ptr2->m_Nodo;
+		ptr2->m_Nodo = temp;
+		ptr = ptr->next;
+		ptr2 = ptr2->next;
+	}
 }
